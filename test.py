d4caabe4510c7dde217e37cb4d6cd14149909125
“Ainara2828” <ainavillars@gmail.com>
2022-10-11 16:37:52 +0200

    function displayNumbers added to test.py on my-branch

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+def displayNumbers(x):
+    for i in range(0,x):
+        print(i)
+
+
 def main():
     print("hello from local")
     print('hello from remote')
+    displayNumbers(8)
   
 main()
